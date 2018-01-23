cbdd08305b157947e982f5ec0f7c6e1f64cf5ae5
srj92 <suraj.gupta5077@gmail.com>
2018-01-24 03:37:36 +0530

    added comments

--- app/routes/queue_routes.js
+++ app/routes/queue_routes.js
@@ -3,22 +3,28 @@ module.exports = function(app) {
     // Our in memory queue object holding all polling tasks
     var queue = {};
 
+    // Task template to store task data
     function Task(inputTimeMillis, taskName, timeOffset) {
         this.inputTimeMillis = inputTimeMillis;
         this.taskName = taskName;
         this.timeOffset = timeOffset;
     }
 
-    /*
-    *  Routes for queue api below
-    */
 
-    // Get all currently polling tasks route
+    /**
+     * Routes for queue api below
+     */
+
     app.get('/all', (req, res) => {
 
+        // Get current timestamp to calculate the remaing time in each task
         const currentRequestTimeMillis = getCurrentTimeMillis();
         var reponseArray = [];
 
+        /**
+         * Iterate the task queue containg all current running tasks, 
+         * calculate remaining time and form response array.
+         */
         for (var task in queue) {
             if(queue.hasOwnProperty(task) ) {
                 const inputTimeMillis = queue[task].inputTimeMillis;
@@ -35,25 +41,27 @@ module.exports = function(app) {
         res.json(reponseArray);
     });
 
-    // Add a polling task route
-    app.post('/queue/:task/:timeOffset', (req, res) => {
-        
-        const currentRequestTimeMillis = getCurrentTimeMillis();
 
+    
+    app.post('/queue/:task/:timeOffset', (req, res) => {
+        const currentRequestTimeMillis = getCurrentTimeMillis();
         var taskName = req.params.task;
         var timeOffset = req.params.timeOffset;
 
+        // Create current task object and store in queue object
         var task = new Task(currentRequestTimeMillis, taskName, timeOffset);
         queue[taskName] = task;
         
-        var response = {name: taskName, done: true};
-
-        setTimeout( function(){ 
+        var response = {name: taskName, done: true}; // Create response object
+       
+        // Return response after timeOffset seconds
+        setTimeout( function() {  
             res.json(response);
         }, timeOffset*1000);
     });
 
-    // Returns current timestamp in milli seconds
+
+    // Helper function. Returns current timestamp in milli seconds
     function getCurrentTimeMillis() {
         const currentRequestTime = new Date(); 
         const currentRequestTimeMillis = Math.round(currentRequestTime.getTime()); 
